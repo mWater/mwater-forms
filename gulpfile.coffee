19ed112ba2dc301c42b59be089875b0e8c12b2a7
Clayton Grassick <clayton@claytronics.org>
2015-02-17 19:41:56 -0500

    Updated and switched gulp coffee

---
+++ gulpfile.coffee
@@ -0,0 +1,38 @@
+gulp = require 'gulp'
+coffee = require 'gulp-coffee' 
+glob = require 'glob'
+extractor = require('ez-localize/extractor')
+source = require 'vinyl-source-stream'
+browserify = require 'browserify'
+coffeeify = require 'coffeeify'
+hbsfy = require 'hbsfy'
+
+# Compile coffeescript to js in lib/
+gulp.task 'coffee', ->
+  gulp.src('./src/*.coffee')
+    .pipe(coffee({ bare: true }))
+    .pipe(gulp.dest('./lib/'))
+
+# Copy non-coffeescript files
+gulp.task 'copy', ->
+  gulp.src(['./src/**/*.js', './src/**/*.hbs'])
+    .pipe(gulp.dest('./lib/'))
+
+gulp.task 'localize', (cb) ->
+  options = { extensions: ['.js', '.coffee'], transform: [coffeeify, hbsfy] }
+  extractor.updateLocalizationFile "src/index.coffee", "localizations.json", options, ->
+    cb()
+
+# Compile tests
+gulp.task 'prepareTests', ->
+  files = glob.sync("./test/*Tests.coffee");
+  bundler = browserify({ entries: files, extensions: [".js", ".coffee"] })
+  stream = bundler.bundle()
+    .pipe(source('browserified.js'))
+    .pipe(gulp.dest('./test/'))
+  return stream
+
+
+gulp.task 'compile', gulp.series('coffee', 'copy', 'localize')
+
+gulp.task 'default', gulp.series('compile')
--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,82 @@
-var gulp = require("gulp");
-var coffee = require('gulp-coffee');
-var gutil = require('gulp-util');
-var browserify = require('browserify');
-var concat = require('gulp-concat');
-var open = require('gulp-open');
-var streamConvert = require('vinyl-source-stream');
-var extractor = require('ez-localize/extractor');
-var coffeeify = require('coffeeify');
-var hbsfy = require('hbsfy');
-var glob = require('glob');
+require('coffee-script').register() 
+require('./gulpfile.coffee')
 
-var EXPRESS_PORT = 8081;
-var DEMO_PAGE = "/demo/demo.html";
+// var gulp = require("gulp");
+// var coffee = require('gulp-coffee');
+// var gutil = require('gulp-util');
+// var browserify = require('browserify');
+// var concat = require('gulp-concat');
+// var open = require('gulp-open');
+// var streamConvert = require('vinyl-source-stream');
+// var extractor = require('ez-localize/extractor');
+// var coffeeify = require('coffeeify');
+// var hbsfy = require('hbsfy');
+// var glob = require('glob');
 
-// Compilation
-gulp.task('coffee', ['localize'], function() {
-	gulp.src('./src/*.coffee')
-		.pipe(coffee({ bare: true }).on('error', gutil.log))
-		.pipe(gulp.dest('./lib/'));
-});
+// var EXPRESS_PORT = 8081;
+// var DEMO_PAGE = "/demo/demo.html";
 
-gulp.task('copy', function() {
-	gulp.src(['./src/**/*.js', './src/**/*.css', './src/**/*.hbs'])
-		.pipe(gulp.dest('./lib/'));
-});
+// // Compilation
+// gulp.task('coffee', ['localize'], function() {
+// 	gulp.src('./src/*.coffee')
+// 		.pipe(coffee({ bare: true }).on('error', gutil.log))
+// 		.pipe(gulp.dest('./lib/'));
+// });
 
-gulp.task('localize', function(done) {
-	var options = { extensions: ['.js', '.coffee'], transform: [coffeeify, hbsfy] };
-	extractor.updateLocalizationFile("src/index.coffee", "localizations.json", options, function() { 
-		done();
-	});
-});
+// gulp.task('copy', function() {
+// 	gulp.src(['./src/**/*.js', './src/**/*.css', './src/**/*.hbs'])
+// 		.pipe(gulp.dest('./lib/'));
+// });
 
-gulp.task('prepareTests', function() {
-	var files = glob.sync("./test/*Tests.coffee");
-	var bundler = browserify({ entries: files, extensions: [".js", ".coffee"] });
-	var stream = bundler.bundle()
-		.pipe(streamConvert('browserified.js'))
-		.pipe(gulp.dest('./test'))
-	return stream;
-});
+// gulp.task('localize', function(done) {
+// 	var options = { extensions: ['.js', '.coffee'], transform: [coffeeify, hbsfy] };
+// 	extractor.updateLocalizationFile("src/index.coffee", "localizations.json", options, function() { 
+// 		done();
+// 	});
+// });
 
-// Demo
-gulp.task('browserifyDemo', function() {
-	var bundler = browserify('./lib/demo.js');
-	bundler.bundle()
-		.pipe(streamConvert('bundle.js'))
-		.pipe(gulp.dest('./demo'));
-});
+// gulp.task('prepareTests', function() {
+// 	var files = glob.sync("./test/*Tests.coffee");
+// 	var bundler = browserify({ entries: files, extensions: [".js", ".coffee"] });
+// 	var stream = bundler.bundle()
+// 		.pipe(streamConvert('browserified.js'))
+// 		.pipe(gulp.dest('./test'))
+// 	return stream;
+// });
 
-gulp.task('startExpress', function() {
-	var express = require("express");
-	var app = express();
-	app.use(express.static(__dirname));
-	app.use("/", function(req, res) { res.redirect(DEMO_PAGE); } );
-	app.listen(EXPRESS_PORT);
-	console.log("Server started on port " + EXPRESS_PORT);
-});
+// // Demo
+// gulp.task('browserifyDemo', function() {
+// 	var bundler = browserify('./lib/demo.js');
+// 	bundler.bundle()
+// 		.pipe(streamConvert('bundle.js'))
+// 		.pipe(gulp.dest('./demo'));
+// });
 
-gulp.task('watch', function() {
-	gulp.watch('./src/demo.js', ['compile', 'browserifyDemo']);
-	gulp.watch(['./src/*.coffee', './test/*.coffee', './src/templates/*.hbs'], ['compile', 'browserifyDemo']);
-});
+// gulp.task('startExpress', function() {
+// 	var express = require("express");
+// 	var app = express();
+// 	app.use(express.static(__dirname));
+// 	app.use("/", function(req, res) { res.redirect(DEMO_PAGE); } );
+// 	app.listen(EXPRESS_PORT);
+// 	console.log("Server started on port " + EXPRESS_PORT);
+// });
 
-gulp.task('ngrok', function() {
-	require('ngrok').connect(EXPRESS_PORT, function (err, url) {
-		console.log("Public Url is:\n%s", url);
-	});
-});
+// gulp.task('watch', function() {
+// 	gulp.watch('./src/demo.js', ['compile', 'browserifyDemo']);
+// 	gulp.watch(['./src/*.coffee', './test/*.coffee', './src/templates/*.hbs'], ['compile', 'browserifyDemo']);
+// });
 
-gulp.task('openBrowser', function() {
-	var options = { url: "http://localhost:" + EXPRESS_PORT };
-	gulp.src("." + DEMO_PAGE).pipe(open("", options));
-});
+// gulp.task('ngrok', function() {
+// 	require('ngrok').connect(EXPRESS_PORT, function (err, url) {
+// 		console.log("Public Url is:\n%s", url);
+// 	});
+// });
 
-gulp.task('compile', ['coffee', 'copy']);
-gulp.task('demo', ['compile', 'browserifyDemo', 'startExpress', 'watch', 'ngrok', 'openBrowser']);
-gulp.task('default', ['compile']);
+// gulp.task('openBrowser', function() {
+// 	var options = { url: "http://localhost:" + EXPRESS_PORT };
+// 	gulp.src("." + DEMO_PAGE).pipe(open("", options));
+// });
+
+// gulp.task('compile', ['coffee', 'copy']);
+// gulp.task('demo', ['compile', 'browserifyDemo', 'startExpress', 'watch', 'ngrok', 'openBrowser']);
+// gulp.task('default', ['compile']);
