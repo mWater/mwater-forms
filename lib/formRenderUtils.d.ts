155c9756f83d333dd5870a4dd59113b143025432
Rajesh Sharma <broncha@rajesharma.com>
2019-12-25 11:51:21 +0545

    adding formRenderUtils types

---
+++ lib/formRenderUtils.d.ts
@@ -0,0 +1,16 @@
+import { Item, FormDesign, Question, QuestionBase } from "./formDesign"
+import ResponseRow from "./ResponseRow"
+import { Schema } from "mwater-expressions"
+import { RefObject, ReactElement } from "react"
+
+/** Render an item, given its data, visibility function, etc. */
+export function renderItem(
+  item: Item,
+  data: any,
+  responseRow: ResponseRow,
+  schema: Schema,
+  onDataChange: () => any,
+  isVisible: () => boolean,
+  onNext: () => any,
+  ref: RefObject<HTMLElement>
+): ReactElement
