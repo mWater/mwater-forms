39be02373cc02424eeae23ec7e02d522fc81d693
Rajesh Sharma <broncha@rajesharma.com>
2019-12-25 12:59:59 +0545

    fix onDataChange

--- lib/formRenderUtils.d.ts
+++ lib/formRenderUtils.d.ts
@@ -2,6 +2,7 @@ import { Item, FormDesign, Question, QuestionBase } from "./formDesign"
 import ResponseRow from "./ResponseRow"
 import { Schema } from "mwater-expressions"
 import { RefObject, ReactElement } from "react"
+import { ResponseData } from "./response"
 
 /** Render an item, given its data, visibility function, etc. */
 export function renderItem(
@@ -9,7 +10,7 @@ export function renderItem(
   data: any,
   responseRow: ResponseRow,
   schema: Schema,
-  onDataChange: () => any,
+  onDataChange: (data: ResponseData) => void,
   isVisible: () => boolean,
   onNext: () => any,
   ref?: (c: any) => void
