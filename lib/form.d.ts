b2fb48b0e3f189d43300db97e68e0c07f21a98d3
Clayton Grassick <clayton@claytronics.org>
2021-06-28 12:03:36 -0400

    Change to ts

--- lib/form.d.ts
+++ lib/form.d.ts
@@ -1,139 +1,103 @@
-import { Expr } from "mwater-expressions"
-import { FormDesign } from "./formDesign"
-
+import { Expr } from "mwater-expressions";
+import { FormDesign } from "./formDesign";
 /** Survey form */
 export interface Form {
-  _id: string
-  _rev?: number
-
-  /** _id of the form that this is a duplicate of */
-  _basedOn?: string
-
-  /** List of deployments. */
-  deployments?: Deployment[]
-
-  /** Current status of the form. Starts in active state */
-  state: "active" | "deleted"
-
-  /** Design of the form */
-  design: FormDesign
-
-  /** Current mwater-visualization dashboard */
-  dashboard?: any
-
-  /** Indicator calculations that create indicator values from form responses */
-  indicatorCalculations?: IndicatorCalculation[]
-
-  /** True if master form (DEPRECATED) */
-  isMaster?: boolean
-
-  /** If clone form, then _id of master form (DEPRECATED) */
-  masterForm?: boolean
-
-  /** Permissions on the form */
-  roles: FormRole[]
-
-  created: {
-    /** ISO8601 datetime */
-    on: string
-
-    /** User _id */
-    by: string
-  }
-
-  modified?: {
-    /** ISO8601 datetime */
-    on: string
-
-    /** User _id */
-    by: string
-  }
-
-  removed?: {
-    /** ISO8601 datetime */
-    on: string
-
-    /** User _id */
-    by: string
-  }
+    _id: string;
+    _rev?: number;
+    /** _id of the form that this is a duplicate of */
+    _basedOn?: string;
+    /** List of deployments. */
+    deployments?: Deployment[];
+    /** Current status of the form. Starts in active state */
+    state: "active" | "deleted";
+    /** Design of the form */
+    design: FormDesign;
+    /** Current mwater-visualization dashboard */
+    dashboard?: any;
+    /** Indicator calculations that create indicator values from form responses */
+    indicatorCalculations?: IndicatorCalculation[];
+    /** True if master form (DEPRECATED) */
+    isMaster?: boolean;
+    /** If clone form, then _id of master form (DEPRECATED) */
+    masterForm?: boolean;
+    /** Permissions on the form */
+    roles: FormRole[];
+    created: {
+        /** ISO8601 datetime */
+        on: string;
+        /** User _id */
+        by: string;
+    };
+    modified?: {
+        /** ISO8601 datetime */
+        on: string;
+        /** User _id */
+        by: string;
+    };
+    removed?: {
+        /** ISO8601 datetime */
+        on: string;
+        /** User _id */
+        by: string;
+    };
 }
-
 /** Calculates an indicator value for a response of a form */
 export interface IndicatorCalculation {
-  /** Unique id of calculation */
-  _id: string
-
-  /** Id of indicator */
-  indicator: string
-
-  /** Id of roster if from a roster */
-  roster?: string
-
-  /** Map of indicator property id to mwater-expression expression to create value */
-  expressions: { [propertyId: string]: Expr }[]
-
-  /** Optional mwater-expression expression to restrict when to create indicator value */
-  condition?: Expr
-
-  /** Optional mwater-expression expression to select datetime of indicator value. Defaults to submittedOn */
-  datetimeExpr?: Expr
-
-  /** List of ids of users and groups who can view created indicator values i.e. user:<username> or group:<groupname> */
-  viewers: string[]
+    /** Unique id of calculation */
+    _id: string;
+    /** Id of indicator */
+    indicator: string;
+    /** Id of roster if from a roster */
+    roster?: string;
+    /** Map of indicator property id to mwater-expression expression to create value */
+    expressions: {
+        [propertyId: string]: Expr;
+    }[];
+    /** Optional mwater-expression expression to restrict when to create indicator value */
+    condition?: Expr;
+    /** Optional mwater-expression expression to select datetime of indicator value. Defaults to submittedOn */
+    datetimeExpr?: Expr;
+    /** List of ids of users and groups who can view created indicator values i.e. user:<username> or group:<groupname> */
+    viewers: string[];
 }
-
 /** A deployment is a grouping of people who can answer a form and an optional approval chain and viewers. */
 export interface Deployment {
-  _id: string
-
-  /** Name of the deployment */
-  name: string
-
-  /** True if deployment is accepting new responses */
-  active: boolean
-
-  /** True if deployment is public (anonymous responses allowed) */
-  public?: boolean
-
-  /** Contact name and email for person responsible for deployment. Used for public deployments */
-  contactName?: string
-
-  /** Contact name and email for person responsible for deployment. Used for public deployments */
-  contactEmail?: string
-
-  /** List of ids of users and groups who can complete form. i.e. user:<username> or group:<groupname> */
-  enumerators: string[]
-
-  /** List of ids of users and groups who can view final responses. i.e. user:<username> or group:<groupname> */
-  viewers: string[]
-
-  /** List of ids of users and groups who can edit/delete all responses. i.e. user:<username> or group:<groupname> */
-  admins: string[]
-
-  /** List of ids of users and groups who can view all indicator calculation. i.e. user:<username> or group:<groupname> */
-  indicatorCalculationViewers?: string[]
-
-  /** True if enumerator retains admin role on final responses */
-  enumeratorAdminFinal?: boolean
-
-  /** Optional approval stages that responses must pass through to become final. Each stage
-   * has a list of approvers who can approve/reject the response and move it along */
-  approvalStages: ApprovalStage[]
+    _id: string;
+    /** Name of the deployment */
+    name: string;
+    /** True if deployment is accepting new responses */
+    active: boolean;
+    /** True if deployment is public (anonymous responses allowed) */
+    public?: boolean;
+    /** Contact name and email for person responsible for deployment. Used for public deployments */
+    contactName?: string;
+    /** Contact name and email for person responsible for deployment. Used for public deployments */
+    contactEmail?: string;
+    /** List of ids of users and groups who can complete form. i.e. user:<username> or group:<groupname> */
+    enumerators: string[];
+    /** List of ids of users and groups who can view final responses. i.e. user:<username> or group:<groupname> */
+    viewers: string[];
+    /** List of ids of users and groups who can edit/delete all responses. i.e. user:<username> or group:<groupname> */
+    admins: string[];
+    /** List of ids of users and groups who can view all indicator calculation. i.e. user:<username> or group:<groupname> */
+    indicatorCalculationViewers?: string[];
+    /** True if enumerator retains admin role on final responses */
+    enumeratorAdminFinal?: boolean;
+    /** Optional approval stages that responses must pass through to become final. Each stage
+     * has a list of approvers who can approve/reject the response and move it along */
+    approvalStages: ApprovalStage[];
 }
-
 interface FormRole {
-  /** Subject (user:xyz or group:abc or "all") */
-  id: string
-
-  /** view can view only, admin can do anything, deploy cannot edit but can deploy form */
-  role: "view" | "admin" | "deploy"
+    /** Subject (user:xyz or group:abc or "all") */
+    id: string;
+    /** view can view only, admin can do anything, deploy cannot edit but can deploy form */
+    role: "view" | "admin" | "deploy";
 }
-
 /** One stage of approval for a deployment needed */
 interface ApprovalStage {
-  /** List of ids of users and groups who can approve/reject responses at this step. i.e. user:<username> or group:<groupname> */
-  approvers: string[]
-
-  /** If true, prevents approvers from editing/deleting */
-  preventEditing?: boolean
+    /** List of ids of users and groups who can approve/reject responses at this step. i.e. user:<username> or group:<groupname> */
+    approvers: string[];
+    /** If true, prevents approvers from editing/deleting */
+    preventEditing?: boolean;
 }
+export {};
--- lib/response.d.ts
+++ lib/response.d.ts
@@ -1,390 +1,265 @@
 /** Response to a survey form */
 export interface Response {
-  /** Unique id of response */
-  _id: string
-
-  /** Revision of response */
-  _rev?: number
-
-  /** String code of the response. Unique-ish and short enough for human use */
-  code?: string
-
-  /** _id of form that this is for */
-  form: string
-
-  /** _rev of the form this was based on */
-  formRev: number
-
-  /** _id of the deployment that this response is part of */
-  deployment: string
-
-  /** Current successful approval list */
-  approvals?: ResponseApproval[]
-
-  /** User _id who started the response */
-  user?: string | null
-
-  /** Username of user (virtual field) */
-  username?: string | null
-
-  /** Current status. Can go from draft->final if no approval stages */
-  status: "draft" | "pending" | "rejected" | "final"
-
-  /** Rejection message if in rejected state */
-  rejectionMessage?: string
-
-  /** Response data. Organized by question id. */
-  data: ResponseData
-
-  /** Permissions on response */
-  roles: ResponseRole[]
-
-  /** When response was started (ISO8601) */
-  startedOn: string
-
-  /** When response was last submitted (to pending or final) */
-  submittedOn?: string
-
-  /** Name of draft */
-  draftName?: string
-
-  /** Array of entity references in response. */
-  entities?: EntityRef[]
-
-  /** Events that have happened to response */
-  events?: ResponseEvent[]
-
-  // # Array of pending entity creates
-  // pendingEntityCreates: {
-  //   type: "array"
-  //   items: {
-  //     type: "object"
-  //     properties: {
-  //       questionId: { type: "string" }     # Question which is creating the entity
-  //       entityType: { type: "string" }     # Type of the entity
-  //       entity: { type: "object" }         # Entity object to create. _id and _roles already set.
-  //     }
-  //     required: ["questionId", "entityType", "entity"]
-  //     additionalProperties: false
-  //   }
-  // }
-
-  // # Array of pending entity creates
-  // pendingEntityUpdates: {
-  //   type: "array"
-  //   items: {
-  //     type: "object"
-  //     properties: {
-  //       questionId: { type: "string" }     # Question which is creating the entity
-  //       entityType: { type: "string" }     # Type of the entity
-  //       entityId: { type: "string" }       # _id of entity to update
-  //       updates: { type: "object" }        # Entity properties to update
-  //     }
-  //     required: ["questionId", "entityType", "entityId", "updates"]
-  //     additionalProperties: false
-  //   }
-  // }
-
-  created?: {
-    /** ISO8601 datetime */
-    on: string
-
-    /** User _id */
-    by: string
-  }
-
-  modified?: {
-    /** ISO8601 datetime */
-    on: string
-
-    /** User _id */
-    by: string
-  }
-
-  removed?: {
-    /** ISO8601 datetime */
-    on: string
-
-    /** User _id */
-    by: string
-  }
-
-  /** Assignement _id associated to the response */
-  assignment?: string
-  ipAddress?: string
-
-  /** The confidential data if the form has confidential mode true */
-  confidentialData?: ConfidentialData
+    /** Unique id of response */
+    _id: string;
+    /** Revision of response */
+    _rev?: number;
+    /** String code of the response. Unique-ish and short enough for human use */
+    code?: string;
+    /** _id of form that this is for */
+    form: string;
+    /** _rev of the form this was based on */
+    formRev: number;
+    /** _id of the deployment that this response is part of */
+    deployment: string;
+    /** Current successful approval list */
+    approvals?: ResponseApproval[];
+    /** User _id who started the response */
+    user?: string | null;
+    /** Username of user (virtual field) */
+    username?: string | null;
+    /** Current status. Can go from draft->final if no approval stages */
+    status: "draft" | "pending" | "rejected" | "final";
+    /** Rejection message if in rejected state */
+    rejectionMessage?: string;
+    /** Response data. Organized by question id. */
+    data: ResponseData;
+    /** Permissions on response */
+    roles: ResponseRole[];
+    /** When response was started (ISO8601) */
+    startedOn: string;
+    /** When response was last submitted (to pending or final) */
+    submittedOn?: string;
+    /** Name of draft */
+    draftName?: string;
+    /** Array of entity references in response. */
+    entities?: EntityRef[];
+    /** Events that have happened to response */
+    events?: ResponseEvent[];
+    created?: {
+        /** ISO8601 datetime */
+        on: string;
+        /** User _id */
+        by: string;
+    };
+    modified?: {
+        /** ISO8601 datetime */
+        on: string;
+        /** User _id */
+        by: string;
+    };
+    removed?: {
+        /** ISO8601 datetime */
+        on: string;
+        /** User _id */
+        by: string;
+    };
+    /** Assignement _id associated to the response */
+    assignment?: string;
+    ipAddress?: string;
+    /** The confidential data if the form has confidential mode true */
+    confidentialData?: ConfidentialData;
 }
-
 interface ResponseApproval {
-  /** User id who did approval */
-  by: string
-
-  /** When (ISO8601) */
-  on: string
-
-  /** True if was done by an admin, not the normal approver */
-  override?: boolean
+    /** User id who did approval */
+    by: string;
+    /** When (ISO8601) */
+    on: string;
+    /** True if was done by an admin, not the normal approver */
+    override?: boolean;
 }
-
 interface EntityRef {
-  /** Question _id */
-  question: string
-
-  /** e.g. "water_point" */
-  entityType: string
-
-  /** property code (e.g "_id" or "code") of entity that is referenced in value e.g. "code" */
-  property: { type: "string" }
-
-  /** Value of entity property that is referenced */
-  value: any
-
-  /** _id of roster entry, null if not in roster */
-  roster: string | null
+    /** Question _id */
+    question: string;
+    /** e.g. "water_point" */
+    entityType: string;
+    /** property code (e.g "_id" or "code") of entity that is referenced in value e.g. "code" */
+    property: {
+        type: "string";
+    };
+    /** Value of entity property that is referenced */
+    value: any;
+    /** _id of roster entry, null if not in roster */
+    roster: string | null;
 }
-
 interface ResponseRole {
-  /** Subject (user:xyz or group:abc or "all") */
-  id: string
-
-  /** view can view only, admin can do anything */
-  role: "view" | "admin"
+    /** Subject (user:xyz or group:abc or "all") */
+    id: string;
+    /** view can view only, admin can do anything */
+    role: "view" | "admin";
 }
-
 interface ResponseEvent {
-  type: "draft" | "submit" | "approve" | "reject" | "edit"
-
-  /** When event took place */
-  on: string
-
-  /** Can be anonymous otherwise _id of user */
-  by: "string" | "null"
-
-  /** True if approve was done by an admin, not the normal approver */
-  override?: boolean
-
-  /** Rejection message */
-  message?: string
+    type: "draft" | "submit" | "approve" | "reject" | "edit";
+    /** When event took place */
+    on: string;
+    /** Can be anonymous otherwise _id of user */
+    by: "string" | "null";
+    /** True if approve was done by an admin, not the normal approver */
+    override?: boolean;
+    /** Rejection message */
+    message?: string;
 }
-
 /** Data of a response (answers to all questions) */
 export interface ResponseData {
-  [itemId: string]: Answer | RosterData
+    [itemId: string]: Answer | RosterData;
 }
-
 /** Answer to a question */
 export interface Answer {
-  /** Value of the answer */
-  value?: AnswerValue | null
-
-  /** Alternate value if chosen. "value" will be null */
-  alternate?: "na" | "dontknow" | null
-
-  /** Timestamp (for recordTimestamp option) ISO 8601 timestamp */
-  timestamp?: string
-
-  /** Location-stamp (for recordLocation option) */
-  location?: { accuracy?: number; altitude?: number; altitudeAccuracy?: number; longitude: number; latitude: number }
-
-  /** Specify choices values */
-  specify?: {
-    [choiceId: string]: any
-  }
-
-  /** True if answer has been redacted and moved to confidentialData */
-  confidential?: boolean
-
-  /** Comments */
-  comments?: string
-
-  /** Randomly asked questions
-    Stores value in `randomAsked` as boolean. This is a special value that is computed when the item is shown for the first time
-    based on the probability and is not deleted when cleaning the response when the question becomes invisible so it remains stable.
-    This also prevents a loop of hiding the question because it was randomly invisible and then making it visible again and recomputing
-    the randomness. */
-  randomAsked?: boolean
+    /** Value of the answer */
+    value?: AnswerValue | null;
+    /** Alternate value if chosen. "value" will be null */
+    alternate?: "na" | "dontknow" | null;
+    /** Timestamp (for recordTimestamp option) ISO 8601 timestamp */
+    timestamp?: string;
+    /** Location-stamp (for recordLocation option) */
+    location?: {
+        accuracy?: number;
+        altitude?: number;
+        altitudeAccuracy?: number;
+        longitude: number;
+        latitude: number;
+    };
+    /** Specify choices values */
+    specify?: {
+        [choiceId: string]: any;
+    };
+    /** True if answer has been redacted and moved to confidentialData */
+    confidential?: boolean;
+    /** Comments */
+    comments?: string;
+    /** Randomly asked questions
+      Stores value in `randomAsked` as boolean. This is a special value that is computed when the item is shown for the first time
+      based on the probability and is not deleted when cleaning the response when the question becomes invisible so it remains stable.
+      This also prevents a loop of hiding the question because it was randomly invisible and then making it visible again and recomputing
+      the randomness. */
+    randomAsked?: boolean;
 }
-
 /** Data for a roster group/matrix. Array of entries */
-export type RosterData = RosterEntry[]
-
+export declare type RosterData = RosterEntry[];
 /** Data for a single roster group/matrix entry */
 export interface RosterEntry {
-  /** Unique id */
-  _id: string
-
-  /** Answers indexed by item id */
-  data: RosterEntryData
+    /** Unique id */
+    _id: string;
+    /** Answers indexed by item id */
+    data: RosterEntryData;
 }
-
 /** Answers for a single roster entry */
 export interface RosterEntryData {
-  [itemId: string]: Answer
+    [itemId: string]: Answer;
 }
-
 /** Stores the confidential data */
 export interface ConfidentialData {
-  [itemId: string]: ConfidentialAnswer | ConfidentialRosterEntry[]
+    [itemId: string]: ConfidentialAnswer | ConfidentialRosterEntry[];
 }
-
 /** One confidential answer */
 export interface ConfidentialAnswer {
-  /** Value of the answer */
-  value?: AnswerValue | null
-
-  /** Alternate value if chosen. "value" will be null */
-  alternate?: "na" | "dontknow" | null
-
-  /** Specify choices values */
-  specify?: {
-    [choiceId: string]: any
-  }
+    /** Value of the answer */
+    value?: AnswerValue | null;
+    /** Alternate value if chosen. "value" will be null */
+    alternate?: "na" | "dontknow" | null;
+    /** Specify choices values */
+    specify?: {
+        [choiceId: string]: any;
+    };
 }
-
 /** One roster that contains confidential data */
 export interface ConfidentialRosterEntry {
-  /** Id of roster entry */
-  _id: string
-
-  data: ConfidentialRosterEntryData
+    /** Id of roster entry */
+    _id: string;
+    data: ConfidentialRosterEntryData;
 }
-
 /** Confidential data for a single roster entry */
 export interface ConfidentialRosterEntryData {
-  [itemId: string]: ConfidentialAnswer
+    [itemId: string]: ConfidentialAnswer;
 }
-
 /** All values of answers possible */
-export type AnswerValue =
-  | TextAnswerValue
-  | NumberAnswerValue
-  | ChoiceAnswerValue
-  | ChoicesAnswerValue
-  | BooleanAnswerValue
-  | DateAnswerValue
-  | UnitsAnswerValue
-  | LocationAnswerValue
-  | ImageAnswerValue
-  | ImagesAnswerValue
-  | TextsAnswerValue
-  | SiteAnswerValue
-  | EntityAnswerValue
-  | ItemsChoicesAnswerValue
-  | MatrixAnswerValue
-  | AdminRegionAnswerValue
-  | AquagenxCBTAnswerValue
-  | CascadingListAnswerValue
-  | CascadingRefAnswerValue
-
-export type TextAnswerValue = string
-
-export type NumberAnswerValue = number
-
+export declare type AnswerValue = TextAnswerValue | NumberAnswerValue | ChoiceAnswerValue | ChoicesAnswerValue | BooleanAnswerValue | DateAnswerValue | UnitsAnswerValue | LocationAnswerValue | ImageAnswerValue | ImagesAnswerValue | TextsAnswerValue | SiteAnswerValue | EntityAnswerValue | ItemsChoicesAnswerValue | MatrixAnswerValue | AdminRegionAnswerValue | AquagenxCBTAnswerValue | CascadingListAnswerValue | CascadingRefAnswerValue;
+export declare type TextAnswerValue = string;
+export declare type NumberAnswerValue = number;
 /** Choice id */
-export type ChoiceAnswerValue = string
-
+export declare type ChoiceAnswerValue = string;
 /** Choice ids */
-export type ChoicesAnswerValue = string[]
-
-export type BooleanAnswerValue = boolean
-
+export declare type ChoicesAnswerValue = string[];
+export declare type BooleanAnswerValue = boolean;
 /** YYYY-MM-DD */
-export type DateAnswerValue = string
-
+export declare type DateAnswerValue = string;
 export interface UnitsAnswerValue {
-  quantity: number
-  /** id of units */
-  units: string
+    quantity: number;
+    /** id of units */
+    units: string;
 }
-
 export interface LocationAnswerValue {
-  latitude: number
-  longitude: number
-  /** Elevation, taking into account mastHeight and depth if present */
-  altitude?: number
-  accuracy?: number
-  altitudeAccuracy?: number
-  /** Height of mast of GPS device (altitude is GPS altitude - mast height - depth) */
-  mastHeight?: number
-  /** Depth of pipe or other object (altitude is GPS altitude - mast height - depth) */
-  depth?: number
-  /** Method used to set the location */
-  method?: "gps" | "map" | "manual"
+    latitude: number;
+    longitude: number;
+    /** Elevation, taking into account mastHeight and depth if present */
+    altitude?: number;
+    accuracy?: number;
+    altitudeAccuracy?: number;
+    /** Height of mast of GPS device (altitude is GPS altitude - mast height - depth) */
+    mastHeight?: number;
+    /** Depth of pipe or other object (altitude is GPS altitude - mast height - depth) */
+    depth?: number;
+    /** Method used to set the location */
+    method?: "gps" | "map" | "manual";
 }
-
 export interface ImageAnswerValue {
-  /** Id of image (unique) */
-  id: string
-
-  /** Optional caption */
-  caption?: string
+    /** Id of image (unique) */
+    id: string;
+    /** Optional caption */
+    caption?: string;
 }
-
-export type ImagesAnswerValue = ImageAnswerValue[]
-
-export type TextsAnswerValue = string[]
-
+export declare type ImagesAnswerValue = ImageAnswerValue[];
+export declare type TextsAnswerValue = string[];
 export interface SiteAnswerValue {
-  /** Code of site e.g. "10007" */
-  code: string
+    /** Code of site e.g. "10007" */
+    code: string;
 }
-
 /** Entity id is the string value */
-export type EntityAnswerValue = string
-
+export declare type EntityAnswerValue = string;
 export interface ItemsChoicesAnswerValue {
-  /** object of key = item id, value: choice id */
-  [itemId: string]: string
+    /** object of key = item id, value: choice id */
+    [itemId: string]: string;
 }
-
 /** { ITEM1ID: { QUESTION1ID: { value: somevalue }, QUESTION2ID: { ...}}, ITEM2ID: ... } */
 export interface MatrixAnswerValue {
-  [itemId: string]: { [questionId: string]: any }
+    [itemId: string]: {
+        [questionId: string]: any;
+    };
 }
-
 /** Admin region (DEPRECATED) _id of admin region */
-export type AdminRegionAnswerValue = number
-
+export declare type AdminRegionAnswerValue = number;
 /** Answer for a Aquagenx CBT type question */
 export interface AquagenxCBTAnswerValue {
-  cbt: {
-    /** True if compartment 1 is colored */
-    c1: boolean
-    /** True if compartment 2 is colored */
-    c2: boolean
-    /** True if compartment 3 is colored */
-    c3: boolean
-    /** True if compartment 4 is colored */
-    c4: boolean
-    /** True if compartment 5 is colored */
-    c5: boolean
-
-    /** Rating of safety */
-    healthRisk: "safe" | "probably_safe" | "possibly_safe" | "possibly_unsafe" | "probably_unsafe" | "unsafe"
-
-    /** Most probable number of E.Coli per 100ml */
-    mpn: number
-
-    /** Upper 95% Confidence Interval/100ml */
-    confidence: number
-  }
-
-  /** Image of bag */
-  image: ImageAnswerValue
+    cbt: {
+        /** True if compartment 1 is colored */
+        c1: boolean;
+        /** True if compartment 2 is colored */
+        c2: boolean;
+        /** True if compartment 3 is colored */
+        c3: boolean;
+        /** True if compartment 4 is colored */
+        c4: boolean;
+        /** True if compartment 5 is colored */
+        c5: boolean;
+        /** Rating of safety */
+        healthRisk: "safe" | "probably_safe" | "possibly_safe" | "possibly_unsafe" | "probably_unsafe" | "unsafe";
+        /** Most probable number of E.Coli per 100ml */
+        mpn: number;
+        /** Upper 95% Confidence Interval/100ml */
+        confidence: number;
+    };
+    /** Image of bag */
+    image: ImageAnswerValue;
 }
-
 export interface CascadingListAnswerValue {
-  /** ID of the row. Can be undefined temporarily
-   * to force the response cleaner to re-calculate the id
-   * based on the column values.
-   */
-  id?: string
-
-  /** Rest of column values */
-  [columnId: string]: string | undefined
+    /** ID of the row. Can be undefined temporarily
+     * to force the response cleaner to re-calculate the id
+     * based on the column values.
+     */
+    id?: string;
+    /** Rest of column values */
+    [columnId: string]: string | undefined;
 }
-
 /** _id of the row */
-export type CascadingRefAnswerValue = string
+export declare type CascadingRefAnswerValue = string;
+export {};
