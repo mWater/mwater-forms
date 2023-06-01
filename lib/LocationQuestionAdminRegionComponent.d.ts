389b12923a52adf9f25b6b2d2b54281e293966be
Rajesh <broncha@rajesharma.com>
2023-06-01 18:45:16 +0545

    compile

---
+++ lib/LocationQuestionAdminRegionComponent.d.ts
@@ -0,0 +1,12 @@
+import React from "react";
+declare type LocationQuestionAdminRegionComponentProps = {
+    apiUrl?: string;
+    login?: any;
+    location: {
+        longitude: number;
+        latitude: number;
+        method: string;
+    };
+};
+declare const LocationQuestionAdminRegionComponent: React.FC<LocationQuestionAdminRegionComponentProps>;
+export default LocationQuestionAdminRegionComponent;
